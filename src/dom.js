8204700ed56efca92e18b97eb67e7a1d96f96620
eliptik <adamfeyk5@gmail.com>
2023-08-05 18:58:28 +0300

    Add project delete form & update dom.js

--- src/dom.js
+++ src/dom.js
@@ -7,8 +7,25 @@ const todayBtn = document.querySelector("#today-btn")
 const weekBtn = document.querySelector("#week-btn")
 
 export const createDOM = () => {
-    renderTasks(selectedProjectIndex)
+    displayTab("allTasks", "All tasks", "stack")
+    renderProjects() //Display the current projects in the projectList array when the page loads
 }
+
+//Local storage
+if(!localStorage.getItem("todoList")) {
+    saveToLocalStorage();
+} else {
+    loadFromLocalStorage();
+}
+function saveToLocalStorage() {
+    localStorage.setItem("todoList", JSON.stringify(projects.projectList));
+    loadFromLocalStorage();
+}
+function loadFromLocalStorage() {
+    let storedProjectList = JSON.parse(localStorage.getItem("todoList"))
+    projects.projectList = storedProjectList;
+}
+
 //Navbar buttons
 allBtn.addEventListener("click", ()=> displayTab("allTasks", "All tasks", "stack"))
 todayBtn.addEventListener("click", ()=> displayTab("today", "Today", "star"))
@@ -42,6 +59,38 @@ function selectProject(index){
     }
 }
 
+
+function removeAndUpdateSelection(index) {
+    const projectRemoveButtons = document.querySelectorAll(".project-btn .remove-btn")
+    projects.removeProject(index)
+    renderProjects()
+    saveToLocalStorage();
+    if(selectedProjectIndex === "allTasks") {
+        renderTasks("allTasks")
+    } else if(selectedProjectIndex === "today") {
+        renderTasks("today")
+    } else if(selectedProjectIndex === "week") {
+        renderTasks("week")
+    }
+    if(selectedProjectIndex > index) { //if you select a project and remove the one that comes before it, display the same selected project
+        renderTasks(selectedProjectIndex-1)
+        selectProject(selectedProjectIndex-1)
+    } else if(selectedProjectIndex < index){ //if you select a project and remove the one that comes after it, display the same selected project
+        renderTasks(selectedProjectIndex)
+        selectProject(selectedProjectIndex)
+    } else if(selectedProjectIndex === 0){ //if you select the first project and remove it, display the next project
+        renderTasks(index)
+        selectProject(index)
+    } else if(selectedProjectIndex === index){ //if you select the last project and remove it, display the previous project
+        renderTasks(index-1)
+        selectProject(index-1)
+    }
+    if(projectRemoveButtons.length-1 === 0) { //if you remove all projects, display "all tasks" tab
+        displayTab("allTasks", "All tasks", "stack")
+        selectProject("allTasks")
+    }
+}
+
 function renderProjects(){
     const projectContainer = document.querySelector(".project-list")
     projectContainer.innerHTML = "" // clear existing projects display when calling the function more than once
@@ -64,36 +113,14 @@ function renderProjects(){
     const projectRemoveButtons = document.querySelectorAll(".project-btn .remove-btn")
     projectRemoveButtons.forEach((btn, index) => {
         btn.addEventListener("click", (e)=>{
-            projects.removeProject(index)
-            let projectListItem = e.target.closest(".project-btn") // Find the btn's container element: ".project-btn"
-            projectListItem.remove()
-            renderProjects()
-            saveToLocalStorage();
-            if(selectedProjectIndex === "allTasks") {
-                renderTasks("allTasks")
-            } else if(selectedProjectIndex === "today") {
-                renderTasks("today")
-            } else if(selectedProjectIndex === "week") {
-                renderTasks("week")
-            }
-            if(selectedProjectIndex > index) { //if you select a project and remove the one that comes before it, display the same selected project
-                renderTasks(selectedProjectIndex-1)
-                selectProject(selectedProjectIndex-1)
-            } else if(selectedProjectIndex < index){ //if you select a project and remove the one that comes after it, display the same selected project
-                renderTasks(selectedProjectIndex)
-                selectProject(selectedProjectIndex)
-            } else if(selectedProjectIndex === 0){ //if you select the first project and remove it, display the next project
-                renderTasks(index)
-                selectProject(index)
-            } else if(selectedProjectIndex === index){ //if you select the last project and remove it, display the previous project
-                renderTasks(index-1)
-                selectProject(index-1)
-            }
-            if(projectRemoveButtons.length-1 === 0) { //if you remove all projects, display "all tasks" tab
-                displayTab("allTasks", "All tasks", "stack")
-                selectProject("allTasks")
-            }
-            e.stopPropagation();
+            editProjectForm.classList.add("hidden")
+            editTaskForm.classList.add("hidden")
+            deleteProjectForm.classList.remove("hidden")
+            modalOn()
+            modalTitle.textContent = "Delete Project"
+            deleteProjectModalTitle.textContent = `${projects.projectList[index].title}`
+            deleteProjectForm.onsubmit = () => removeAndUpdateSelection(index)
+            e.stopPropagation();     
         })
     })
     //View selected project
@@ -114,11 +141,12 @@ function renderProjects(){
         })
 
     //Edit project title
-    const editProjectButtons = document.querySelectorAll(".edit-btn")
+    const editProjectButtons = document.querySelectorAll(".project-btn .edit-btn")
     editProjectButtons.forEach((btn, index)=> {
         btn.addEventListener("click", (e) => {
             editProjectForm.classList.remove("hidden")
             editTaskForm.classList.add("hidden")
+            deleteProjectForm.classList.add("hidden")
             let projectListItem = e.target.closest(".project-btn")
             projectListItem.classList.add("edit-active")
             projectEditTitleInput.value = projects.projectList[index].title
@@ -248,7 +276,6 @@ taskForm.addEventListener("submit", (e)=> {
     projects.addTask(titleInput.value, taskDate, prioritySelection.value, selectedProjectIndex)
     taskForm.reset()
     hideTaskForm();
-    renderProjects()
     saveToLocalStorage();
     renderTasks(selectedProjectIndex)
 })
@@ -300,6 +327,7 @@ editProjectForm.addEventListener("submit", (e)=> {
         renderProjects()
         saveToLocalStorage();
         renderTasks(selectedProjectIndex)
+        selectProject(selectedProjectIndex)
         projectEditTitleInput.value = ""
     }
     editProjectForm.classList.add("hidden")
@@ -327,11 +355,11 @@ editTaskForm.addEventListener("submit", (e)=> {
     const selectedItem = document.querySelector(".edit-active")
     if(selectedItem) {
         projects.editTask(selectedItem.dataset.parentProjectIndex, selectedItem.dataset.taskIndex, taskEditTitleInput.value, newDate, priorityEditInput.value)
-        renderProjects()
         saveToLocalStorage();
         renderTasks(selectedProjectIndex)
     }
     editTaskForm.classList.add("hidden")
+    deleteProjectForm.classList.add("hidden")
     editTaskForm.reset()
     modalOff()
 })
@@ -340,6 +368,32 @@ taskCancelBtn.addEventListener("click", (e) => {
     const taskListItems = document.querySelectorAll(".task-item-container")
     taskListItems.forEach(item => item.classList.remove("edit-active"))
     editTaskForm.classList.add("hidden")
+    modalOff()
+})
+
+//Delete project form
+const deleteProjectForm = document.querySelector("#delete-project-form")
+const deleteProjectModalTitle = document.querySelector("#delete-project-form span")
+const cancelDeleteBtn = document.querySelector("#form-delete-cancel")
+deleteProjectForm.classList.add("hidden")
+deleteProjectForm.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    const selectedItem = document.querySelector(".edit-active")
+    if(selectedItem) {
+        projects.removeProject(selectedItem.dataset.projectIndex)
+        renderProjects()
+        saveToLocalStorage();
+        renderTasks(selectedProjectIndex)
+    }
+    editProjectForm.classList.add("hidden")
+    modalOff()
+})
+cancelDeleteBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    projectEditTitleInput.value = ""
+    const projectListItems = document.querySelectorAll(".project-btn")
+    projectListItems.forEach(item => item.classList.remove("edit-active"))
+    editProjectForm.classList.add("hidden")
     modalOff()
 })
 
@@ -357,6 +411,7 @@ function modalOff(){
     overlay.classList.remove("active")
     editProjectForm.classList.add("hidden")
     editTaskForm.classList.add("hidden")
+    deleteProjectForm.classList.add("hidden")
     const projectListItems = document.querySelectorAll(".project-btn")
     const taskListItems = document.querySelectorAll(".task-item-container")
     projectListItems.forEach(item => item.classList.remove("edit-active"))
@@ -394,21 +449,3 @@ function closeMenu() {
     hamburgerMenu.checked = false
     menuOff();
 }
-
-//Local storage
-if(!localStorage.getItem("todoList")) {
-    saveToLocalStorage();
-} else {
-    loadFromLocalStorage();
-}
-function saveToLocalStorage() {
-    localStorage.setItem("todoList", JSON.stringify(projects.projectList));
-    loadFromLocalStorage();
-}
-function loadFromLocalStorage() {
-    let storedProjectList = JSON.parse(localStorage.getItem("todoList"))
-    projects.projectList = storedProjectList;
-}
-
-renderProjects() //Display the default projects in the projectList array when the page loads
-selectProject(0) //Select the first project if exists when the page loads
